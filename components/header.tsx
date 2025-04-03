ef4ef5f3368440b1b4f6fb444ca4bbe41b8bbbb4
Cremacious <chrismackall3@gmail.com>
2025-04-03 15:26:19 -0400

    Added user to data with get current user

--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,13 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 
-// import { checkUser } from '@/lib/checkUser';
+import { checkUser } from '@/lib/checkUser';
 
 const Header = async () => {
-//   const user = await checkUser();
+  const user = await checkUser();
 
   return (
-    <nav className='navbar'>
-      <div className='navbar-container'>
+    <nav className="navbar">
+      <div className="navbar-container">
         <h2>Expense Tracker</h2>
         <div>
           <SignedOut>
